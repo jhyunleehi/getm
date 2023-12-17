d08eeac12367fad0c8a3ce1b8c1ab9eae75b630b
jhyunleehi <jhyunleehi@naver.com>
2023-12-17 23:11:38 +0900

    readme

--- pyCode/code.py
+++ pyCode/code.py
@@ -1,5 +1,17 @@
 print("hello, world!")
 
-a=123
+marks = [90, 25, 67, 45, 80]
+for m in marks:
+    #if m < 60:   continue
+    print("%d번 학생 축하합니다. %d 합격입니다." % (marks.index(m),m))
 
-print(a)
+
+marks = [90, 25, 67, 45, 80]
+for n in range(len(marks)) :    
+    print("%d번 학생 축하합니다. %d 합격입니다." % (n, marks[n]))
+
+marks.sort()
+print(marks)
+
+
+
